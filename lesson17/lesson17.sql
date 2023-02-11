d704ea7a344ed652aa2dce33168d0bb74f1521af
InGev <ingev123@gmail.com>
2023-02-11 15:03:41 +0200

    Lesson17-fin

--- lesson17/lesson17.sql
+++ lesson17/lesson17.sql
@@ -1,2 +1,15 @@
-select count(distinct(JOB_ID)) from employees;
-select  avg(SALARY) as SALARY_AVG, count(EMPLOYEE_ID) as EMPLOYEE_COUNT from employees where DEPARTMENT_ID=90;
+--1
+SELECT COUNT(distinct(JOB_ID)) as COUNT_JOB FROM employees;
+--2
+SELECT  avg(SALARY) as SALARY_AVG, COUNT(EMPLOYEE_ID) as EMPLOYEE_COUNT FROM employees where DEPARTMENT_ID=90;
+--3
+SELECT JOB_ID,COUNT(EMPLOYEE_ID) as EMPLOYEE_COUNT FROM employees GROUP BY JOB_ID;
+--4
+SELECT employees.FIRST_NAME, employees.LAST_NAME, employees.DEPARTMENT_ID, departments.DEPARTMENT_NAME FROM employees
+left join departments on employees.DEPARTMENT_ID = departments.DEPARTMENT_ID;
+--5
+SELECT first_name, last_name, job_id, department_id
+FROM employees
+JOIN departments USING (department_id)
+JOIN locations USING (location_id)
+WHERE city = 'London';
